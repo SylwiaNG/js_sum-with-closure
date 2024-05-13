f4632de2564989a67e405d5f462297ca3a10fb2b
SylwiaNG <iwon9@poczta.onet.pl>
2024-05-13 12:33:00 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,27 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  function innerSum(a, b) {
+    return a + b;
+  }
+
+  return function getSum(a) {
+    callCount++;
+
+    if (callCount <= 3 || callCount % 2 !== 0) {
+      // First three calls and odd calls work fine
+      return function(b) {
+        return innerSum(a, b);
+      };
+    } else {
+      // Even calls return error message
+      return function(b) {
+        return 'Bzzz... Error!';
+      };
+    }
+  };
 }
 
 module.exports = makeRobotAccountant;
